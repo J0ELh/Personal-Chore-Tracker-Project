aecb36ebe182c8c009f0f0552ebae4b9f7c98ec1
jhempel <joelhempel35@gmail.com>
2022-11-11 18:15:24 -0500

    testing commit from new laptop

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,6 +3,6 @@ package ui;
 public class Main {
     public static void main(String[] args) {
         new ChoreTrackerMenu();
-        //test commit to main after deleting testbranch
+        //test commit from new laptop
     }
 }
