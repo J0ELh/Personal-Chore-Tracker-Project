c6f483086fc70857a596cdd2585c28f43ceae825
jhempel <joelhempel35@gmail.com>
2022-12-01 22:16:39 -0800

    reformatted comments for classes and reformatted spacing to adhere to checkstyle

--- src/main/persistance/Writable.java
+++ src/main/persistance/Writable.java
@@ -3,6 +3,7 @@ package persistance;
 import org.json.JSONObject;
 //NOTE: ALL OF THIS CODE IS STRONGLY BASED ON THE CODE PROVIDED BY JsonSerializationDemo provided on edx from our UBC
 //CPSC 210 course team.
+
 //Object that is writable to JSON
 public interface Writable {
     // EFFECTS: returns this as JSON object
